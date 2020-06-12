cc685abf29f4c8b2fd66bd3425ddc916f891eb8e
Mitraws <m.ws@outlook.com>
2020-06-12 17:26:55 +0200

    Add share content

--- App.js
+++ App.js
@@ -1,6 +1,37 @@
 import React from "react";
 import GameScreen from "./src/screens/GameScreen";
+import { Button } from "react-native";
 
 export default function App() {
-  return <GameScreen />;
+  const share = async (color) => {
+    try {
+      const result = await Share.share({
+        message: `Check out this wonderful color: ${color}`,
+      });
+
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          console.log("shared with activity type of", result.activityType);
+        } else {
+          console.log("shared");
+        }
+      } else if (result.action === Share.dismissedAction) {
+        console.log("dismissed");
+      }
+    } catch (error) {
+      Alert.alert(error.message);
+      console.log("failed sharing:", error);
+    }
+  };
+  return (
+    <View>
+      <GameScreen />;
+      <Button
+        title="Share this color!"
+        onPress={() => {
+          share(color);
+        }}
+      />
+    </View>
+  );
 }
