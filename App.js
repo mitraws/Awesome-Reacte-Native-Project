f44d99561a65dbad2a9b515148c3206cd6630369
Mitraws <m.ws@outlook.com>
2020-06-12 15:22:12 +0200

    Replace mapping images with FlatList

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, { useRef, useState } from "react";
 import {
+  FlatList,
   TextInput,
   TouchableOpacity,
-  ScrollView,
   Button,
   Alert,
   Text,
@@ -11,30 +11,29 @@ import {
   Image,
 } from "react-native";
 
+function MyHeader() {
+  return (
+    <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
+      <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
+        Hello React Native
+      </Text>
+      <ActivityIndicator
+        size="large"
+        color="#c1262c"
+        style={{ marginBottom: 30 }}
+      />
+    </View>
+  );
+}
+
 export default function App() {
   const [inputText, setInputText] = useState("");
   const randomNum = useRef(Math.random()).current;
 
-  return (
-    <ScrollView>
-      <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
-        <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
-          Hello React Native
-        </Text>
-        <ActivityIndicator
-          size="large"
-          color="#c1262c"
-          style={{ marginBottom: 30 }}
-        />
-        <TouchableOpacity onPress={() => Alert.alert("Learning RN is so easy")}>
-          <Image
-            source={{
-              uri: `https://picsum.photos/500/300?random=${randomNum}`,
-            }}
-            style={{ width: "100%", height: 160, marginBottom: 30 }}
-          />
-        </TouchableOpacity>
-        <View
+  function MyFooter() {
+    return (
+      <View>
+        <Text
           style={{
             borderWidth: 2,
             borderColor: "black",
@@ -42,8 +41,8 @@ export default function App() {
             marginBottom: 30,
           }}
         >
-          <Text>Hello again!</Text>
-        </View>
+          Hello again!
+        </Text>
         <Button
           onPress={() => Alert.alert(`You typed: ${inputText}`)}
           title="Learn More"
@@ -55,22 +54,30 @@ export default function App() {
           onChangeText={(text) => setInputText(text)}
           placeholder="type here"
         ></TextInput>
-        {[0, 1, 2, 3, 4].map((i) => {
-          return (
-            <TouchableOpacity
-              key={i}
-              onPress={() => Alert.alert("Learning RN is so easy")}
-            >
-              <Image
-                source={{
-                  uri: `https://picsum.photos/500/300?random=${randomNum + i}`,
-                }}
-                style={{ width: "100%", height: 160, marginBottom: 30 }}
-              />
-            </TouchableOpacity>
-          );
-        })}
       </View>
-    </ScrollView>
+    );
+  }
+
+  return (
+    <FlatList
+      ListHeaderComponent={MyHeader}
+      data={[0, 1, 2, 3, 4]}
+      renderItem={({ item }) => {
+        return (
+          <TouchableOpacity
+            onPress={() => Alert.alert(`You pressed image #${item + 1}`)}
+          >
+            <Image
+              source={{
+                uri: `https://picsum.photos/500/300?random=${randomNum + item}`,
+              }}
+              style={{ width: "100%", height: 160, marginBottom: 30 }}
+            />
+          </TouchableOpacity>
+        );
+      }}
+      keyExtractor={(item) => String(item)}
+      ListFooterComponent={MyFooter}
+    />
   );
 }
