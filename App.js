cae2b025745dbe2d10be0813c443349b73c0ca5d
Mitraws <m.ws@outlook.com>
2020-06-12 13:29:14 +0200

    Add button

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { useRef } from "react";
 import {
+  Button,
+  Alert,
   Text,
   View,
   ActivityIndicator,
@@ -10,31 +12,36 @@ export default function App() {
   const randomNum = useRef(Math.random()).current;
 
   return (
-      <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
-        <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
-          Hello React Native
-        </Text>
-        <ActivityIndicator
-          size="large"
-          color="#c1262c"
-          style={{ marginBottom: 30 }}
-        />
-        <Image
-          source={{
-            uri: `https://picsum.photos/500/300?random=${randomNum}`,
-          }}
-          style={{ width: "100%", height: 160, marginBottom: 30 }}
-        />
-        <View
-          style={{
-            borderWidth: 2,
-            borderColor: "black",
-            padding: 20,
-            marginBottom: 30,
-          }}
-        >
-          <Text>Hello again!</Text>
-        </View>
+    <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
+      <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
+        Hello React Native
+      </Text>
+      <ActivityIndicator
+        size="large"
+        color="#c1262c"
+        style={{ marginBottom: 30 }}
+      />
+      <Image
+        source={{
+          uri: `https://picsum.photos/500/300?random=${randomNum}`,
+        }}
+        style={{ width: "100%", height: 160, marginBottom: 30 }}
+      />
+      <View
+        style={{
+          borderWidth: 2,
+          borderColor: "black",
+          padding: 20,
+          marginBottom: 30,
+        }}
+      >
+        <Text>Hello again!</Text>
       </View>
+      <Button
+        onPress={() => Alert.alert("Learning RN is so easy")}
+        title="Learn More"
+        color="#c1262c"
+      />
+    </View>
   );
 }
