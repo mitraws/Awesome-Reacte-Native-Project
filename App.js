8280ea38a3ac2c8128c4d2021c4e50e973b7c313
Mitraws <m.ws@outlook.com>
2020-06-12 13:35:58 +0200

    Wrap each Image element in TouchableOpacity

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import {
+  TouchableOpacity,
   ScrollView,
   Button,
   Alert,
@@ -14,48 +15,54 @@ export default function App() {
 
   return (
     <ScrollView>
-    <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
-      <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
-        Hello React Native
-      </Text>
-      <ActivityIndicator
-        size="large"
-        color="#c1262c"
-        style={{ marginBottom: 30 }}
-      />
-      <Image
-        source={{
-          uri: `https://picsum.photos/500/300?random=${randomNum}`,
-        }}
-        style={{ width: "100%", height: 160, marginBottom: 30 }}
-      />
-      <View
-        style={{
-          borderWidth: 2,
-          borderColor: "black",
-          padding: 20,
-          marginBottom: 30,
-        }}
-      >
-        <Text>Hello again!</Text>
+      <View style={{ marginHorizontal: 40, marginVertical: 60 }}>
+        <Text style={{ fontWeight: "bold", fontSize: 24, marginBottom: 30 }}>
+          Hello React Native
+        </Text>
+        <ActivityIndicator
+          size="large"
+          color="#c1262c"
+          style={{ marginBottom: 30 }}
+        />
+        <TouchableOpacity onPress={() => Alert.alert("Learning RN is so easy")}>
+          <Image
+            source={{
+              uri: `https://picsum.photos/500/300?random=${randomNum}`,
+            }}
+            style={{ width: "100%", height: 160, marginBottom: 30 }}
+          />
+        </TouchableOpacity>
+        <View
+          style={{
+            borderWidth: 2,
+            borderColor: "black",
+            padding: 20,
+            marginBottom: 30,
+          }}
+        >
+          <Text>Hello again!</Text>
+        </View>
+        <Button
+          onPress={() => Alert.alert("Learning RN is so easy")}
+          title="Learn More"
+          color="#c1262c"
+        />
+        {[0, 1, 2, 3, 4].map((i) => {
+          return (
+            <TouchableOpacity
+              onPress={() => Alert.alert("Learning RN is so easy")}
+            >
+              <Image
+                key={i}
+                source={{
+                  uri: `https://picsum.photos/500/300?random=${randomNum + i}`,
+                }}
+                style={{ width: "100%", height: 160, marginBottom: 30 }}
+              />
+            </TouchableOpacity>
+          );
+        })}
       </View>
-      <Button
-        onPress={() => Alert.alert("Learning RN is so easy")}
-        title="Learn More"
-        color="#c1262c"
-      />
-      {[0, 1, 2, 3, 4].map(i => {
-  return (
-    <Image
-      key={i}
-      source={{
-        uri: `https://picsum.photos/500/300?random=${randomNum + i}`
-      }}
-      style={{ width: "100%", height: 160, marginBottom: 30 }}
-    />
-  );
-})}
-    </View>
     </ScrollView>
   );
 }
